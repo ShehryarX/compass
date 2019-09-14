4fff3b9e6ad6021ed0bad41b1bd182691b4147f4
Shahbaz Momi <shahbaz.man3@gmail.com>
2019-09-14 11:01:01 -0400

    Started base IOS app

---
+++ WanderIOS/Wander/EventView.swift
@@ -0,0 +1,64 @@
+//
+//  EventView.swift
+//  Wander
+//
+//  Created by Shahbaz on 2019-09-14.
+//  Copyright © 2019 Shahbaz Momi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+@IBDesignable
+class EventView: UIView {
+        
+    @IBOutlet var contentView: UIView!
+    @IBOutlet var topTitle: UILabel!
+    @IBOutlet var subtitle: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var body1: UILabel!
+    @IBOutlet var body2: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        basicInit()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        basicInit()
+    }
+    
+    func basicInit() {
+//        contentView = Bundle.main.loadNibNamed("EventView", owner: self, options: nil)![0] as! UIView
+        Bundle.main.loadNibNamed("EventView", owner: self, options: nil)
+        contentView.fixInView(self)
+    }
+    
+}
+
+extension UIView
+{
+    func fixInView(_ container: UIView!) -> Void{
+        self.translatesAutoresizingMaskIntoConstraints = false;
+        self.frame = container.frame;
+        container.addSubview(self);
+        NSLayoutConstraint(item: self, attribute: .leading, relatedBy: .equal, toItem: container, attribute: .leading, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .trailing, relatedBy: .equal, toItem: container, attribute: .trailing, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .top, relatedBy: .equal, toItem: container, attribute: .top, multiplier: 1.0, constant: 0).isActive = true
+        NSLayoutConstraint(item: self, attribute: .bottom, relatedBy: .equal, toItem: container, attribute: .bottom, multiplier: 1.0, constant: 0).isActive = true
+    }
+}
+
+extension UIView {
+
+    // Using a function since `var image` might conflict with an existing variable
+    // (like on `UIImageView`)
+    func asImage() -> UIImage {
+        let renderer = UIGraphicsImageRenderer(bounds: bounds)
+        return renderer.image { rendererContext in
+            layer.render(in: rendererContext.cgContext)
+        }
+    }
+    
+}
---
+++ WanderIOS/Wander/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Wander
+//
+//  Created by Shahbaz on 2019-09-14.
+//  Copyright © 2019 Shahbaz Momi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var event: EventView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
