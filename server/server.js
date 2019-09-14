020b5037ab4f7913e2fdc6b5a7ecb9532686a272
Shahbaz Momi <shahbaz.man3@gmail.com>
2019-09-14 03:11:19 -0400

    Fix

--- server/server.js
+++ server/server.js
@@ -3,30 +3,92 @@ const graph = require("fbgraph");
 
 const { fbAuthKey } = require("./config/keys");
 
-graph.setAccessToken("383823849195100|f6BhOUU5n5Kw-1e9gU1V9UZpxl8");
+var app = express();
+var server = require("http").createServer(app);
 
-graph.get("Shehryar Assad", (err, res) => {
-  console.log(res);
-});
-
-var authUrl = graph.getOauthUrl({
+var conf = {
   client_id: fbAuthKey.id,
-  redirect_uri: "http://www.google.com"
+  client_secret: fbAuthKey.secret,
+  scope: "email, user_about_me, user_birthday, user_location, publish_actions",
+  redirect_uri: "shehryar.me"
+};
+
+// Configuration
+var methodOverride = require("method-override");
+var bodyParser = require("body-parser");
+var errorHandler = require("errorhandler");
+
+app.set("views", __dirname + "/views");
+// Jade was renamed to pug
+app.set("view engine", "pug");
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(methodOverride());
+
+var path = require("path");
+app.use(express.static(path.join(__dirname, "/public")));
+
+var env = process.env.NODE_ENV || "development";
+if ("development" == env) {
+  app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+}
+
+// Routes
+
+app.get("/auth", function(req, res) {
+  // we don't have a code yet
+  // so we'll redirect to the oauth dialog
+  if (!req.query.code) {
+    console.log("Performing oauth for some user right now.");
+
+    var authUrl = graph.getOauthUrl({
+      client_id: conf.client_id,
+      redirect_uri: conf.redirect_uri,
+      scope: conf.scope
+    });
+
+    if (!req.query.error) {
+      //checks whether a user denied the app facebook login/permissions
+      res.redirect(authUrl);
+    } else {
+      //req.query.error == 'access_denied'
+      res.send("access denied");
+    }
+  }
+  // If this branch executes user is already being redirected back with
+  // code (whatever that is)
+  else {
+    console.log(
+      "Oauth successful, the code (whatever it is) is: ",
+      req.query.code
+    );
+    // code is set
+    // we'll send that and get the access token
+    graph.authorize(
+      {
+        client_id: conf.client_id,
+        redirect_uri: conf.redirect_uri,
+        client_secret: conf.client_secret,
+        code: req.query.code
+      },
+      function(err, facebookRes) {
+        res.redirect("/UserHasLoggedIn");
+      }
+    );
+  }
 });
 
-// shows dialog
-res.redirect(authUrl);
+// user gets sent here after being authorized
+app.get("/UserHasLoggedIn", function(req, res) {
+  res.render("index", {
+    title: "Logged In"
+  });
+});
 
-// after user click, auth `code` will be set
-// we'll send that and get the access token
-graph.authorize(
-  {
-    client_id: conf.client_id,
-    redirect_uri: conf.redirect_uri,
-    client_secret: conf.client_secret,
-    code: req.query.code
-  },
-  function(err, facebookRes) {
-    res.redirect("/loggedIn");
-  }
-);
+var port = process.env.PORT || 3000;
+app.listen(port, function() {
+  console.log("Express server listening on port %d", port);
+});
