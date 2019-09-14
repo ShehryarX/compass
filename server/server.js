5f152c69f7eb6eba5c867d4c21b6cd940c6d5ff7
RafitJ <rafit.jamil@gmail.com>
2019-09-14 12:54:25 -0400

    server update

--- server/server.js
+++ server/server.js
@@ -1,94 +1,106 @@
 const express = require("express");
 const graph = require("fbgraph");
 
-const { fbAuthKey } = require("./config/keys");
+const {
+    fbAuthKey
+} = require("./config/keys");
+const gl = require('geolocation-utils');
 
 var app = express();
 var server = require("http").createServer(app);
 
 var conf = {
-  client_id: fbAuthKey.id,
-  client_secret: fbAuthKey.secret,
-  scope: "email, user_about_me, user_birthday, user_location, publish_actions",
-  redirect_uri: "shehryar.me"
+    client_id: fbAuthKey.id,
+    client_secret: fbAuthKey.secret,
+    scope: "email, user_about_me, user_birthday, user_location, publish_actions",
+    redirect_uri: "shehryar.me"
 };
 
-// Configuration
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 var errorHandler = require("errorhandler");
 
-app.set("views", __dirname + "/views");
-// Jade was renamed to pug
-app.set("view engine", "pug");
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-);
-app.use(methodOverride());
+const user_id = 2505011072909984
+const access_token = 'EAAFdFfQwBlwBAOWKyaoeKUsWhAnWVOS5uROF6yqJmLVy2fpz5peZB3I5f8mLW65hW6ZBtf5zs6EZBmdeFRrZCAc1FRMdMVsZA9vDBPSWrZBlyAXk5obl88N2e8thQrBUHJxRZB9ipbQtqLMmZAk9eBybI0ZChOolBkE2ZBEcSKyaMfxGR2TeuZCv7yoZCA4khBJ72BJLfdZBGFVTYbQZDZD'
 
-var path = require("path");
-app.use(express.static(path.join(__dirname, "/public")));
-
-var env = process.env.NODE_ENV || "development";
-if ("development" == env) {
-  app.use(errorHandler({ dumpExceptions: true, showStack: true }));
+let geolocation = {
+    lat: 43.4767,
+    lon: -80.5388
 }
 
-// Routes
+graph.setAccessToken(access_token);
 
-app.get("/auth", function(req, res) {
-  // we don't have a code yet
-  // so we'll redirect to the oauth dialog
-  if (!req.query.code) {
-    console.log("Performing oauth for some user right now.");
+app.get("/", function(req, res) {
+    const center = geolocation
+    const radius = 10000
 
-    var authUrl = graph.getOauthUrl({
-      client_id: conf.client_id,
-      redirect_uri: conf.redirect_uri,
-      scope: conf.scope
+    graph.get(`${user_id}?fields=id,events`, function(err, graph_res) {
+        var events_array = graph_res.events.data.filter((e) => {
+            if (e.place === undefined || e.place.location === undefined) return false
+            var event_loc = {
+                lat: e.place.location.latitude,
+                lon: e.place.location.longitude
+            }
+            return gl.insideCircle(event_loc, center, radius)
+        })
+        res.send(events_array)
+    })
+
+});
+
+app.get("/event_data", function(req, res) {
+    const center = geolocation
+    const radius = 10000
+
+    graph.get(`${user_id}?fields=id,events`, function(err, graph_res) {
+        var events_array = graph_res.events.data.filter((e) => {
+            if (e.place === undefined || e.place.location === undefined) return false
+            var event_loc = {
+                lat: e.place.location.latitude,
+                lon: e.place.location.longitude
+            }
+            return gl.insideCircle(event_loc, center, radius)
+        })
+        res.send(events_array)
+    })
+});
+
+let event_dict = {}
+
+const smth = e => new Promise(resolve => {
+    graph.get(`${e.id}?fields=id,events`, function(err, friends_res) {
+        friends_res.events.data.forEach((i) => {
+            id = i.id
+            if (event_dict[id]) {
+                event_dict[id].total += 1
+                event_dict[id].friends.push(e.id)
+            } else {
+                event_dict[id] = {
+                    total: 1,
+                    friends: [e.id],
+                }
+            }
+        })
+        resolve()
+    })
+})
+
+app.get("/friends", async function(req, res) {
+    event_dict = {}
+    await new Promise((resolve, reject) => {
+        graph.get(`${user_id}?fields=id,friends`, async function(err, graph_res) {
+            for (let i = 0; i < graph_res.friends.data.length; i++) {
+                await smth(graph_res.friends.data[i])
+            }
+
+            resolve();
+        })
+
     });
 
-    if (!req.query.error) {
-      //checks whether a user denied the app facebook login/permissions
-      res.redirect(authUrl);
-    } else {
-      //req.query.error == 'access_denied'
-      res.send("access denied");
-    }
-  }
-  // If this branch executes user is already being redirected back with
-  // code (whatever that is)
-  else {
-    console.log(
-      "Oauth successful, the code (whatever it is) is: ",
-      req.query.code
-    );
-    // code is set
-    // we'll send that and get the access token
-    graph.authorize(
-      {
-        client_id: conf.client_id,
-        redirect_uri: conf.redirect_uri,
-        client_secret: conf.client_secret,
-        code: req.query.code
-      },
-      function(err, facebookRes) {
-        res.redirect("/UserHasLoggedIn");
-      }
-    );
-  }
+    return res.status(200).json(event_dict);
 });
-
-// user gets sent here after being authorized
-app.get("/UserHasLoggedIn", function(req, res) {
-  res.render("index", {
-    title: "Logged In"
-  });
-});
-
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
-  console.log("Express server listening on port %d", port);
+    console.log("Express server listening on port %d", port);
 });
