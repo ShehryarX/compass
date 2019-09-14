cc745340b5cb17ac9880a4063858f7a77909ee0f
RafitJ <rafit.jamil@gmail.com>
2019-09-14 03:35:40 -0400

    recommit server files and temp server

--- flask/app.py
+++ flask/app.py
@@ -4,16 +4,22 @@ import requests
 
 app = Flask(__name__)
 
+base_url = "https://graph.facebook.com/"
+token = 'EAAFdFfQwBlwBAANy4ZA1eQHPc5ZAYOBP9pEXCrboVuLi54uwGrT7iZAQDBsGG7EB7mtCZBNTNEq3BIZCD66H9j89sgCim4UlPLSsGwRSYB14QK7d4VCCPI8sZBx3MGvJxt2cOgjIyZBEpTpMRnGrTuBHokI834oQazPij9kLvLZAObZAZAWmdrtHjDDfxehZBj516vOBmcijPqkwTJfO0sldSCX'
 
-@app.route("/<int:user_id>")
-def func(user_id):
-    base_url = "https://graph.facebook.com/"
-    token = 'EAAchvkxiC3gBAPvKlOX2vvSafBM9HlnufNcd501XPxliJ3TTX3YjSDfgEv8JjiHPH4yfVNoXgvDCX0lufiGJSdjsINoFnZBHkk4wq4vkT2dSCbPpo4zRRydkdFZCy3LHTyrAweXOovH3ET06MfseGDuFsZANhZBlV9Fxe11pGN9XjJ1fpl0SZAVFhNSMKCC9J4huZCCaJo7QZDZD'
+@app.route("/user/<int:user_id>")
+def user(user_id):
     fieldsUrl = "fields=id,name,likes,friends,groups,location,hometown,gender,age_range,address,businesses,events"
     url = base_url + str(user_id) + "?" + fieldsUrl + "&access_token=" + token
-    print(url)
     return requests.get(url).content
 
+
+@app.route("/event/<int:event_id>")
+def event(event_id):
+    fieldsUrl = "?fields=id,name,category,decsription,parent_group,roles"
+    url = base_url + str(event_id) + fieldsUrl + "&access_token=" + token
+    print(url)
+    return requests.get(url).content
 
 if __name__ == "__main__":
     app.run()
