94055a9afb7bd9364898f4c47c9121733d266eb7
RafitJ <rafit.jamil@gmail.com>
2019-09-14 21:20:46 -0400

    finished fixing event_dict

--- server/find.js
+++ server/find.js
@@ -9,7 +9,7 @@ module.exports.textToLongLat = async function(text) {
         const response = await axios.get(findURL, {
             params: {
                 key: API_KEY,
-                input: text,
+                input: removeNumbers(text) + " waterloo",
                 inputtype: "textquery"
             }
         });
@@ -20,53 +20,41 @@ module.exports.textToLongLat = async function(text) {
                 place_id: place_id,
             }
         })
+        const location = response2.data.result.geometry.location
+        return location;
     } catch (error) {
         console.error(error);
     }
-
-    .then(function(response) {
-
-            .then(function(response) {
-                    const location = response.data.result.geometry.location
-                    return location;
-                })
-                .catch(function(error) {
-                    console.log(error);
-                })
-        })
-        .catch(function(error) {
-            console.log(error);
-        })
 }
 
+const removeNumbers = str => str.replace(/[0-9]/g, '');
 
-
-module.exports.textToLocation = function(text) {
-    axios.get(findURL, {
+module.exports.textToLocation = async function(text) {
+    try {
+        const response = await axios.get(findURL, {
             params: {
                 key: API_KEY,
-                input: text,
+                input: removeNumbers(text) + " waterloo",
                 inputtype: "textquery"
             }
+        });
+        const place_id = response.data.candidates[0].place_id
+        const response2 = await axios.get(placeURL, {
+            params: {
+                key: API_KEY,
+                place_id: place_id,
+            }
         })
-        .then(function(response) {
-            const place_id = response.data.candidates[0].place_id
-            axios.get(placeURL, {
-                    params: {
-                        key: API_KEY,
-                        place_id: place_id,
-                    }
-                })
-                .then(function(response) {
-                    const placeAPI = response.data.result.geometry.location
-                    console.log(response.data.result)
-                    return (placeAPI);
-                })
-                .catch(function(error) {
-                    console.log(error);
-                })
-        })
-        .catch(function(error) {
-            console.log(error);
-        })
+        const lat = response2.data.result.geometry.location.lat
+        const long = response2.data.result.geometry.location.lng
+        const street = response2.data.result.formatted_address
+
+        return {
+            street,
+            lat,
+            long
+        };
+    } catch (error) {
+        console.error(error);
+    }
 }
--- server/server.js
+++ server/server.js
@@ -52,7 +52,6 @@ app.get("/user_events", function(req, res) {
 
     graph.get(`${user_id}?fields=id,events`, function(err, graph_res) {
         var events_array = graph_res.events.data.filter((e) => {
-            console.log(e.name)
             return verifyEvent(e, center)
         })
         res.send(events_array)
@@ -60,21 +59,28 @@ app.get("/user_events", function(req, res) {
 
 });
 
-function verifyEvent(e, geo) {
+async function verifyEvent(e, geo) {
     const radius = 10000
     const start_time = e.start_time.split('T')[0]
+    if (e.end_time === undefined) {
+        return false
+    }
+    const end_time = e.end_time.split('T')[0]
     const e_date = Date.parse(start_time)
+    const e_date_end = Date.parse(end_time)
     const fiveDays = 1000 * 60 * 60 * 24 * 5;
-    if ((e_date - c_date) > fiveDays || e_date < c_date) return false
 
+    if ((e_date - c_date) > fiveDays || ((e_date < c_date) && (e_date_end < c_date))) return false
+
+    let lat, lng;
     if (e.place.location === undefined) {
-        const {
-            lat,
-            lng
-        } = findAPI.getLongLat(e.place.name)
+        const response = await findAPI.textToLongLat(e.place.name);
+        lat = response.lat;
+        lng = response.lng;
+
     } else {
-        const lat = e.place.location.latitude
-        const lng = e.place.location.longitude
+        lat = e.place.location.latitude
+        lng = e.place.location.longitude
     }
     const event_loc = {
         lat: lat,
@@ -86,32 +92,31 @@ function verifyEvent(e, geo) {
 let event_dict = {}
 
 const friendEvents = (personId, isUser, geo) => new Promise(resolve => {
-    graph.get(`${personId}?fields=id,events`, function(err, res) {
-        for (let i = 0; i < res.events.data.length; i++) {
-            const event = res.events.data[i]
-            console.log(event)
-            if (!(verifyEvent(event, geo))) continue
-            id = event.id
-            if (event_dict[id] && !(isUser)) {
-                event_dict[id].total_friends += 1
-                event_dict[id].friends.push(personId)
-            } else if (isUser) {
-                event_dict[id] = {
-                    total_friends: 0,
-                    friends: [],
-                    details: event
-                }
-            } else {
-                event_dict[id] = {
-                    total_friends: 1,
-                    friends: [personId],
-                    details: event
+    graph.get(`${personId}/events/?fields=id,name,place,attending_count,about,cover,description,end_time,start_time,type`, async function(err, res) {
+        if (res.data !== undefined) {
+            for (let i = 0; i < res.data.length; i++) {
+                const event = res.data[i]
+                if (!(await (verifyEvent(event, geo)))) continue
+                id = event.id
+                if (event_dict[id] && !(isUser)) {
+                    event_dict[id].total_friends += 1
+                    event_dict[id].friends.push(personId)
+                } else if (isUser) {
+                    event_dict[id] = {
+                        total_friends: 0,
+                        friends: [],
+                        details: event
+                    }
+                } else {
+                    event_dict[id] = {
+                        total_friends: 1,
+                        friends: [personId],
+                        details: event
+                    }
                 }
             }
-        }
-        res.events.data.forEach((i) => {
 
-        })
+        }
         resolve()
     })
 })
@@ -132,19 +137,67 @@ app.get("/friends_events", async function(req, res) {
     }
     const center = geolocation
     event_dict = {}
-    await new Promise((resolve, reject) => {
+    await new Promise(async(resolve, reject) => {
         graph.get(`${user_id}?fields=id,friends`, async function(err, graph_res) {
             await friendEvents(user_id, true, center);
             for (let i = 0; i < graph_res.friends.data.length; i++) {
                 await friendEvents(graph_res.friends.data[i].id, false, center);
             }
             resolve();
-        })
 
+        })
     });
 
-    return res.status(200).json(event_dict);
+    new_dict = await fixDict(event_dict)
+
+    return res.status(200).json(new_dict);
 });
+
+let new_dict = {}
+
+function isHappeningNow(info) {
+    start_time = Date.parse(info.start_time.split('T')[0])
+    end_time = Date.parse(info.start_time.split('T')[0])
+    curr_time = c_date
+    if ((curr_time < end_time) && (curr_time > start_time)) return true
+    return false
+}
+
+async function generateLocation(info) {
+    if (info.place.location) {
+        return {
+            street: info.place.location.street,
+            lat: info.place.location.latitude,
+            long: info.place.location.longitude,
+        }
+    }
+    location = await findAPI.textToLocation(info.place.name)
+    return location
+}
+
+async function fixDict(event_dict) {
+    for (var key in event_dict) {
+        if (event_dict.hasOwnProperty(key)) {
+            const data = event_dict[key]
+            const info = data.details
+            const event_piece = {
+                name: info.name,
+                description: info.description,
+                attending_count: info.attending_count,
+                cover_image: info.cover.source,
+                start_time: info.start_time.split('T')[0],
+                end_time: info.end_time.split('T')[0],
+                happening_now: isHappeningNow(info),
+                total_friends_going: data.total_friends,
+                mutual_friends: data.friends,
+                venue_name: info.place.name,
+                location: await generateLocation(info)
+            }
+            new_dict[key] = event_piece
+        }
+    }
+    return new_dict
+}
 
 var port = process.env.PORT || 3000;
 app.listen(port, function() {
